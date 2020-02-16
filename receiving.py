583de41c8677e8ede98ed268f221255a0f884bca
Winson Gin <winsongin@csu.fullerton.edu>
2020-02-16 14:19:28 -0800

    Initial commit

---
+++ receiving.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+# def curTime(): 
+#     now = datetime.now()
+
+#     currentTime = now.strftime("%H:%M:%S") 
+#     print("Current Time =", currentTime)
+    
+
+def promptUser(): 
+    workerId = input("Enter your workerId: ") 
+    print(workerId)
+
+promptUser()
+# curTime()
+
+
+
+
